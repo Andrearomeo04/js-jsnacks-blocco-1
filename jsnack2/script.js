f6c1b39081a1d8079c53c36071415c26bb8a199a
andrearomeo04 <pisco004@gmail.com>
2024-07-01 23:53:42 +0200

    [UPD] added contition to add odd numbers in the array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,4 +2,10 @@ let odd_numbers = [];
 
 for(let i = 0; i < 6; i++) {
     let num = prompt("inserisci un numero qui sotto")
+
+    if (num % 2 !== 0) {
+        odd_numbers.push(`${num}`);
+    }
 }
+
+console.log(`${odd_numbers}`)
