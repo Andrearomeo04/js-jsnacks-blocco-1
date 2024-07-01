8d994bc2bd2fca65488e4d45c7258c398fbcf50c
andrearomeo04 <pisco004@gmail.com>
2024-07-02 00:22:03 +0200

    [UPD] cicled the array, added somma and converted strings into numbers

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,11 @@
 let four_digit = prompt("inserisci un numero a 4 cifre qui sotto")
 
 let digits = four_digit.split('')
+
+let somma = 0
+
+for(let i = 0; i < digits.length; i++) {
+    somma += parseInt(digits[i])
+}
+
+console.log(somma)
