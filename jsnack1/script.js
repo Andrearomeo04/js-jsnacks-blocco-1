81426d375e27e297f36b6efc2cd31a237a336112
andrearomeo04 <pisco004@gmail.com>
2024-07-01 23:09:48 +0200

    [UPD] added event listener, input value and control variable

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,9 @@
-const array = ["andrea", "giacomo", "francesco", "leonardo", "alessandro", "paolo", "giulio", "filippo", "davide", "manuel"]
+const name_list = ["andrea", "giacomo", "francesco", "leonardo", "alessandro", "paolo", "giulio", "filippo", "davide", "manuel"]
 
+let btn = document.getElementById('check');
+
+btn.addEventListener('click', function () {
+    let name = document.getElementById('list').ariaValueMax.toLowerCase();
+
+    let list_check = false
+})
