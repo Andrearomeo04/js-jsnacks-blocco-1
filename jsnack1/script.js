0ed367584827027fb00dedb10689d74ac8f3c500
andrearomeo04 <pisco004@gmail.com>
2024-07-01 23:13:31 +0200

    [UPD] cicled the array and added condition

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,7 +3,12 @@ const name_list = ["andrea", "giacomo", "francesco", "leonardo", "alessandro", "
 let btn = document.getElementById('check');
 
 btn.addEventListener('click', function () {
-    let name = document.getElementById('list').ariaValueMax.toLowerCase();
+    let name = document.getElementById('list').value.toLowerCase();
 
     let list_check = false
+
+    for(i = 0; i<name_list.length; i++) {
+        if (name_list[i].toLowerCase() === name)
+            list_check = true
+    }
 })
